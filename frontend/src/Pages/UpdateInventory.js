b4342db4ad8c8cabc3b6d9ed962a16aeb56a1e7c
srhak <srhak@tamu.edu>
2022-12-03 14:46:08 -0600

    fixed dining dollars typo

---
+++ frontend/src/Pages/UpdateInventory.js
@@ -0,0 +1,23 @@
+// react
+import { useContext, useEffect, useState } from "react";
+
+// external imports
+import { TextField,Button } from "@mui/material";
+import InputAdornment from '@mui/material/InputAdornment';
+
+// components
+import Header from "../Components/Header";
+import TranslatedText from "../Components/TranslatedText";
+
+// pages
+
+// contexts
+import { UserContext } from "../contexts/user";
+import { LanguageContext } from '../contexts/language';
+
+
+const UpdateInventory = () => {
+    
+}
+
+export default UpdateInventory;
